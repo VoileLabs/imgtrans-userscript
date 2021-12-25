bfd9014bcafeaa7f834341ddb798741ff7396a48
QiroNT <i@ntz.im>
2021-12-25 21:10:13 +0800

    chore: desc

--- src/banner.js
+++ src/banner.js
@@ -1,9 +1,13 @@
 // ==UserScript==
-// @name         Manga Translator
-// @namespace    https://touhou.ai/imgtrans/
+// @name         Touhou.AI | Manga Translator
+// @name:zh-CN   Touhou.AI | 图片翻译器
+// @namespace    https://github.com/VoileLabs/imgtrans-userscript
 // @version      0.2.0
-// @description  (WIP) Userscript for https://touhou.ai/imgtrans/
+// @description  (WIP) Userscript for https://touhou.ai/imgtrans/, translate images on Pixiv.
+// @description:zh-CN (WIP) https://touhou.ai/imgtrans/ 的用户脚本版本，一键翻译 Pixiv 的图片
 // @author       QiroNT
+// @license      MIT
+// @supportURL   https://github.com/VoileLabs/imgtrans-userscript
 // @include      http*://www.pixiv.net*
 // @match        http://www.pixiv.net/
 // @connect      i.pximg.net
